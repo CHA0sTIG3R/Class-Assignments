357383ef7b9a99455049f56e1efc258934db7669
CHA0sTIG3R <olowuhamzat@gmail.com>
2021-09-24 16:43:09 -0400

    minimal changes

--- src/Assignment3/Triangle.java
+++ src/Assignment3/Triangle.java
@@ -4,7 +4,7 @@ import Assignment1.*;
 public class Triangle {
     double s1, s2, s3;
 
-    Triangle(double s1, double s2, double s3){
+    public Triangle(double s1, double s2, double s3){
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
