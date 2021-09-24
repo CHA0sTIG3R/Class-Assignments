3ce24f9b1fc806e1602b5b934eac0d75684d66c8
CHA0sTIG3R <olowuhamzat@gmail.com>
2021-09-24 15:44:42 -0400

    problem 2

---
+++ src/Assignment3/Problem2.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        double s1, s2, s3;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number for Side1: ");
+        s1 = input.nextDouble(); // assign input to a variable
+        System.out.println("Enter a number for Side2: ");
+        s2 = input.nextDouble(); // assign input to a variable
+        System.out.println("Enter a number for Side3: ");
+        s3 = input.nextDouble(); // assign input to a variable
+
+        Triangle tri = new Triangle(s1, s2, s3);
+
+        if (!tri.isValid(s1, s2, s3)){
+            System.out.println("Invalid Input");
+        }
+        else{
+            System.out.println(tri.area(s1, s2, s3));
+        }
+    }
+}
---
+++ src/Assignment3/Triangle.java
@@ -0,0 +1,65 @@
+package Assignment3;
+import Assignment1.*;
+
+public class Triangle {
+    double s1, s2, s3;
+
+    Triangle(double s1, double s2, double s3){
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+    }
+    /**
+     * Uses the {@code distance()} from {@linkplain problem1} to calculate the sides of the triangle
+     * and assigns them to the class variables {@code s1}, {@code s2} and {@code s3}
+     * 
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x3
+     * @param y3
+     */
+    public void distPoints(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this.s1 = problem1.distance(x1, y1, x2, y2);
+        this.s2 = problem1.distance(x2, y2, x3, y3);
+        this.s3 = problem1.distance(x3, y3, x1, y1);
+    }
+
+    public boolean isValid(double s1, double s2, double s3) {
+        // check if a + b > c
+        if ((s1 + s2 > s3) && (s2 + s3 > s1) && (s3 + s1 > s2)){
+            // returns true if true
+            return true;
+        }
+        else{
+            // returns false otherwise
+            return false;
+        }
+    }
+    
+    /**
+     * Returns the perimeter of a triangle
+     * 
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return {@code s1} + {@code s2} + {@code s3}
+     */
+    public double perimeter(double s1, double s2, double s3) {
+        return s1+s2+s3;
+    }
+
+    /**
+     * Returns the area of a triangle
+     * 
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return √({@code s}*({@code s}- {@code s1})*({@code s}-{@code s2})*({@code s}-{@code s3}))
+     */
+    public double area(double s1, double s2, double s3) {
+        double s = (s1 + s2 + s3)/2;
+        return Math.sqrt(s*(s-s1)*(s-s2)*(s-s3));
+    }
+}
