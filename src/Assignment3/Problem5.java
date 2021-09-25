a45204fc4f321a44761c714e2aee7083b209c3b4
CHA0sTIG3R <olowuhamzat@gmail.com>
2021-09-24 20:50:49 -0400

    problem 5

---
+++ src/Assignment3/Problem5.java
@@ -0,0 +1,60 @@
+package Assignment3;
+
+
+import java.util.Arrays;
+import java.util.TreeMap;
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        int num, counter = 0, count = 0;
+        int[] arr = new int[100];
+        int[] unq = new int[100];
+
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.print("Enter a number between 1 and 100 (enter 0 to stop): ");
+            num = input.nextInt();
+            counter++;
+            arr[counter] = num;
+            if (!Arrays.asList(unq).contains(num)){
+                count++;
+                unq[count] = num;
+            }
+            if (num == 0){
+                break;
+            }
+        }
+
+        numCount(arr, unq);
+    }
+
+    public static void numCount(int[] arr, int[] unq) {
+        
+        TreeMap<Integer, Integer> mydict = new TreeMap<>();
+        for (int i = 0; i < unq.length; i++){
+            mydict.put(unq[i], 0);
+            for (int j = 0; j < arr.length; j++){
+                if (unq[i] == arr[j]){
+                    mydict.replace(unq[i], mydict.get(unq[i]) + 1);
+                }
+            }
+        }
+        mydict.remove(0);
+
+        for (int num : mydict.keySet()) {
+            
+            int cnt = mydict.get(num);
+
+            System.out.print(num +" occurs " + cnt);
+            if (cnt == 1){
+                System.out.println(" time");
+            }
+            else{
+                System.out.println(" times");
+            }
+        }
+        
+    }
+}
